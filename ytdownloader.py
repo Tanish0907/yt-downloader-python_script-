1f1c9ff4d0effde8eff700b259037696c3858d6b
tarun.kumar <tarun.kumar@bigohtech.com>
2023-08-23 00:43:02 +0530

    1080p download

--- ytdownloader.py
+++ ytdownloader.py
@@ -3,25 +3,52 @@ import pytube.contrib.playlist as pl
 import sys
 import os
 import moviepy.editor as mp
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
+
+global path
+
+def combine(vid_path,audio_path):
+    vids=[]
+    audio=[]
+    for file_path in os.listdir(vid_path):
+        # check if current file_path is a file
+        if os.path.isfile(os.path.join(vid_path, file_path)):
+            # add filename to list
+            vids.append(os.path.join(vid_path, file_path))
+    for file_path in os.listdir(audio_path):
+        # check if current file_path is a file
+        if os.path.isfile(os.path.join(audio_path, file_path)):
+            # add filename to list
+            audio.append(os.path.join(audio_path, file_path))
+    for i in range(len(vids)):
+        video_clip = VideoFileClip(vids[i])
+        audio_clip = AudioFileClip(audio[i])
+        final_clip = video_clip.set_audio(audio_clip)
+        final_clip.write_videofile(vids[i] + ".mp4")	
+        os.remove(vids[i])
+        os.remove(audio[i])
+    os.rmdir(path+".mp3")
+    
 
 
 def vid(s):
 
     video = YouTube(s)
-    print(video)
-    global path
+
     path = "./"+video.title+"/"
     os.makedirs(path)
     print("title of video:", video.title)
     print("length of video:", format(video.length/60, ".2f"), "minutes")
     print("no of views:", video.views)
-    video = video.streams.get_highest_resolution()
-    video.download(path)
+    vid = video.streams.filter(res="1080p").first()
+    audio=video.streams.filter(mime_type="audio/mp4",abr="128kbps").first()
+    audio.download(path+".mp3")
+    vid.download(path)
 
 
 def plst(s):
     playlist = pl.Playlist(s)
-    global path
+
     path = "./"+playlist.title+"/"
     os.makedirs(path)
     print("title of playlist:", playlist.title)
@@ -33,19 +60,46 @@ def plst(s):
         print("title of video:", video.title)
         print("length of video:", format(video.length/60, ".2f"), "minutes")
         print("no of views:", video.views)
-        video = video.streams.get_highest_resolution()
+        vid = video.streams.filter(res="1080p").first()
+        audio=video.streams.filter(mime_type="audio/mp4",abr="128kbps").first()
+
         try:
-            video.download(path)
+            vid.download(path)
+            audio.download(path+".mp3")
         except:
             pass
 
-def music(path):
-    os.mkdir("./songs")
-    for i in os.listdir(path):
-        clip = mp.VideoFileClip(f'{path}/{i}')
-        i=i.replace(".mp4",".mp3")
-        clip.audio.write_audiofile(f'./songs/{i}')
+def music(s):
+    video = YouTube(s)
 
+    path = "./"+video.title+"/"
+    os.makedirs(path)
+    print("title of video:", video.title)
+    print("length of video:", format(video.length/60, ".2f"), "minutes")
+    print("no of views:", video.views)
+    audio=video.streams.filter(mime_type="audio/mp4",abr="128kbps").first()
+    audio.download(path)
+
+def music_plst(s):
+    playlist = pl.Playlist(s)
+
+    path = "./"+playlist.title+"/"
+    os.makedirs(path)
+    print("title of playlist:", playlist.title)
+    print("no of videos:", playlist.length)
+    video = playlist.video_urls
+    for i in video:
+        print("".center(50, "-"))
+        video = YouTube(i)
+        print("title of video:", video.title)
+        print("length of video:", format(video.length/60, ".2f"), "minutes")
+        print("no of views:", video.views)
+        audio=video.streams.filter(mime_type="audio/mp4",abr="128kbps").first()
+
+        try:
+            audio.download(path)
+        except:
+            pass
 
 
 def search(s):
@@ -109,19 +163,19 @@ def main():
         link = input("enter link of vid/plst:")
         if ("list=" in link):
             plst(link)
+            combine(path,path+".mp3")
             print("finished".center(50, "-"))
         else:
             vid(link)
+            combine(path,path+".mp3")
             print("finished".center(50, "-"))
     elif sys.argv[1]=="-m":
-        link = input("enter link of music plst:")
+        link = input("enter link of music plst/vid:")
         if ("list=" in link):
-            plst(link)
-            music(path)
+            music_plst(link)
             print("finished".center(50, "-"))
         else:
-            vid(link)
-            music(path)
+            music(link)
             print("finished".center(50, "-"))
         
 

e0cec8491016f068f75ae059641ee18ef6aa2c4a
tarun.kumar <tarun.kumar@bigohtech.com>
2023-08-26 14:22:07 +0530

    just some fixes

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,13 +1,13 @@
-from pytube import YouTube, Search
+from pytube import YouTube
 import pytube.contrib.playlist as pl
 import sys
 import os
-import moviepy.editor as mp
-from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
-
-global path
-
+import ffmpeg
+import shutil
+global x
 def combine(vid_path,audio_path):
+    output_path="./video_downloaded"
+    os.mkdir(output_path)
     vids=[]
     audio=[]
     for file_path in os.listdir(vid_path):
@@ -21,34 +21,48 @@ def combine(vid_path,audio_path):
             # add filename to list
             audio.append(os.path.join(audio_path, file_path))
     for i in range(len(vids)):
-        video_clip = VideoFileClip(vids[i])
-        audio_clip = AudioFileClip(audio[i])
-        final_clip = video_clip.set_audio(audio_clip)
-        final_clip.write_videofile(vids[i] + ".mp4")	
+        audio_input_file = audio[i]
+        video_input_file = vids[i]
+        output_file =  vids[i].split("/")[-1]
+
+        # Load the audio and video streams
+        audio_stream = ffmpeg.input(audio_input_file)
+        video_stream = ffmpeg.input(video_input_file)
+
+        # Merge the audio and video streams
+        output_stream = ffmpeg.output(video_stream, audio_stream, output_file, vcodec='copy', acodec='aac', strict='experimental')
+
+        # Run the FFmpeg command
+        ffmpeg.run(output_stream)
+        file=f"./{output_file}"
+        destination=output_path
+        shutil.move(file,destination)
         os.remove(vids[i])
         os.remove(audio[i])
     os.rmdir(path+".mp3")
+    os.rmdir(path)
     
 
 
 def vid(s):
-
+    global path
     video = YouTube(s)
-
     path = "./"+video.title+"/"
     os.makedirs(path)
     print("title of video:", video.title)
     print("length of video:", format(video.length/60, ".2f"), "minutes")
     print("no of views:", video.views)
-    vid = video.streams.filter(res="1080p").first()
+    vid = video.streams.filter(res="1080p",progressive="False").first()
+    if vid==None:
+        vid = video.streams.filter(res="720p",progressive="False").first()
     audio=video.streams.filter(mime_type="audio/mp4",abr="128kbps").first()
     audio.download(path+".mp3")
     vid.download(path)
 
 
 def plst(s):
+    global path
     playlist = pl.Playlist(s)
-
     path = "./"+playlist.title+"/"
     os.makedirs(path)
     print("title of playlist:", playlist.title)
@@ -61,6 +75,8 @@ def plst(s):
         print("length of video:", format(video.length/60, ".2f"), "minutes")
         print("no of views:", video.views)
         vid = video.streams.filter(res="1080p").first()
+        if vid==None:
+            vid = video.streams.filter(res="720p",progressive="False").first()
         audio=video.streams.filter(mime_type="audio/mp4",abr="128kbps").first()
 
         try:
@@ -71,7 +87,6 @@ def plst(s):
 
 def music(s):
     video = YouTube(s)
-
     path = "./"+video.title+"/"
     os.makedirs(path)
     print("title of video:", video.title)
@@ -102,64 +117,60 @@ def music_plst(s):
             pass
 
 
-def search(s):
-    global x, lk
-    search = Search(s)
-    lk = []
+# def search(s):
+#     global x, lk
+#     search = Search(s)
+#     lk = []
+#     x=0
+#     for i in search.results:
+#         lk.append(f"https://www.youtube.com/watch?v={i.video_id}")
+#     l = len(lk)
+#     if x > 0:
+#         lk.clear()
+#         search.get_next_results()
+#         for i in search.results:
+#             lk.append(f"https://www.youtube.com/watch?v={i.video_id}")
+#         for i in range(0, l-1):
+#             lk.pop(i)
+#             # print("test",lk[i])
+#         lk.pop()
+#     count = 0
+#     print("search results".center(50, "-"))
+#     for i in lk:
+#         try:
+#             v = YouTube(i)
+#             print(count, v.title)
+#             count = count+1
+#         except:
+#             pass
+#     print("done".center(50, "-"))
 
-    for i in search.results:
-        lk.append(f"https://www.youtube.com/watch?v={i.video_id}")
-    l = len(lk)
-    if x > 0:
-        lk.clear()
-        search.get_next_results()
-        for i in search.results:
-            lk.append(f"https://www.youtube.com/watch?v={i.video_id}")
-        for i in range(0, l-1):
-            lk.pop(i)
-            # print("test",lk[i])
-        lk.pop()
-    count = 0
-    print("search results".center(50, "-"))
-    for i in lk:
-        try:
-            v = YouTube(i)
-            print(count, v.title)
-            count = count+1
-        except:
-            pass
-    print("done".center(50, "-"))
-
-    x = x+1
+#     x = x+1
 
 
-def s2():
-    dall = input(
-        "press r to search or y to download all (AFTER SEARCHING!!)or n to exit or enter the number of video to download:")
-    if dall == "y":
-        for i in lk:
-            vid(i)
-            print("finished".center(50, "-"))
-        exit()
-    elif dall == "n":
-        exit()
-    elif dall == "r":
-        search(sys.argv[2])
-        s2()
-    else:
-        dall = int(dall)
-        vid(lk[dall])
-        print("finished".center(50, "-"))
-        exit()
+# def s2():
+#     dall = input(
+#         "press r to search or y to download all (AFTER SEARCHING!!)or n to exit or enter the number of video to download:")
+#     if dall == "y":
+#         for i in lk:
+#             vid(i)
+#             print("finished".center(50, "-"))
+#         exit()
+#     elif dall == "n":
+#         exit()
+#     elif dall == "r":
+#         search(sys.argv[2])
+#         s2()
+#     else:
+#         dall = int(dall)
+#         vid(lk[dall])
+#         print("finished".center(50, "-"))
+#         exit()
+
 
 
 def main():
-    global x
-    x = 0
-    if sys.argv[1] == "-s":
-
-        s2()
-    elif sys.argv[1] == "-l":
+    if sys.argv[1] == "-l":
         link = input("enter link of vid/plst:")
         if ("list=" in link):
             plst(link)
